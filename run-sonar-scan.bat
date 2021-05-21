8e4c23ead0fbcff38f581529587e546b9519d3fd
Azat Satklichov <azat.satklichov@broadcom.com>
2021-05-21 15:24:28 +0200

    update

---
+++ run-sonar-scan.bat
@@ -0,0 +1,3 @@
+sonar-scanner.bat -Dsonar.organization=asatklichov -Dsonar.projectKey=fe-publish2 -Dsonar.sources=.  -Dsonar.language=ts -Dsonar.host.url=https://sonarcloud.io  -Dsonar.login=4c00fb38417a390016ebff1612ae1e9757614542
+
+#sonar-scanner.bat -Dsonar.organization=eclipse -Dsonar.projectKey=eclipse_che-che4z-lsp-for-cobol-TS -Dsonar.sources=.  -Dsonar.host.url=https://sonarcloud.io  -Dsonar.login=4c00fb38417a390016ebff1612ae1e9757614542
