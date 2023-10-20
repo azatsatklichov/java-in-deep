786a33bbb0bf0baabdb1d92d5282c7a222808f2c
Azat Satklichov <azat.satklichov@broadcom.com>
2023-10-20 20:36:43 +0200

    update

---
+++ src/DatesUntil.java
@@ -0,0 +1,26 @@
+
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.Year;
+import java.util.stream.Stream;
+
+public class DatesUntil {
+
+    public static void main(String... args) {
+        LocalDate start = LocalDate.of(1977, 10, 6);
+        Stream<LocalDate> dates = start.datesUntil(start.plusDays(7));
+        dates.forEach(System.out::println);  
+
+
+        LocalDate birthday = LocalDate.of(1977, 10, 6);
+
+        long leapYears = birthday
+                .datesUntil(LocalDate.now(), Period.ofYears(1))
+                .map(d -> Year.of(d.getYear()))
+                .filter(Year::isLeap)
+                .count();
+
+        System.out.printf("%d leap years since Azat was born on %s", leapYears, birthday);
+    }
+}
---
+++ src/net/sahet/programming/paradigms/zCompletableFutureAPIImprovements.java
@@ -0,0 +1,66 @@
+package net.sahet.programming.paradigms;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * CompletableFuture API Improvements
+ * 
+ * A Future that may be explicitly completed
+ *
+ * <pre>
+CompletableFuture class was introduced in Java 8 to represent the Future which can be completed by setting 
+its value and status explicity. It can be used as java.util.concurrent.CompletionStage. 
+
+It supports dependent functions and actions which got triggered upon the future's completion. 
+
+In java 9 CompletableFuture API has been enhanced further. Following are the relevant changes done to the API.
+
+Support for delays and timeouts.
+Improved support for sub classing.
+New factory methods added.
+ * 
+ * </pre>
+ */
+public class zCompletableFutureAPIImprovements {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.out.println(" Creating a Completed CompletableFuture");
+		CompletableFuture<String> cf = CompletableFuture.completedFuture("message");
+		if (cf.isDone()) {
+			String msg = cf.getNow(null);
+			System.out.println("--> "+msg);
+		}
+
+		System.out.println("Running a Simple Asynchronous Stage");
+		CompletableFuture<Void> cf2 = CompletableFuture.runAsync(() -> {
+			if (Thread.currentThread().isDaemon()) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
+		if (!cf2.isDone()) {
+			Thread.sleep(2000);
+		}
+
+		if (cf2.isDone()) {
+			System.out.println("finally");
+		}
+
+		// This example will show how to delay the completion of a CompletableFuture
+		// with a specific value by one second.
+		CompletableFuture<Object> future = new CompletableFuture<>();
+		future.completeAsync(() -> "input", CompletableFuture.delayedExecutor(1, TimeUnit.SECONDS));
+
+		System.out.println("See also ProcessAPIImprovements#onExit");
+		System.out.println("See also Reactivestreams - nonblocking API");
+
+	}
+
+}
